4dd7efc9343726b5f7595aa6dbd6d7088d4726e5
yaoxiaole <yaoniansong@163.com>
2024-08-22 11:01:40 +0800

    feat: update

--- vite.config.ts
+++ vite.config.ts
@@ -14,47 +14,26 @@ export default defineConfig({
       ext: '.gz', // 压缩文件后缀名
     })
 ],
-  build: {
-    terserOptions: {
-      compress: {
-        //生产环境时移除console
-        drop_console: true,
-        drop_debugger: true,
-      },
+build: {
+  chunkSizeWarningLimit: 1000, // 单个模块文件大小限制1000KB
+  terserOptions: {
+    // 清除代码中console和debugger
+    compress: {
+      drop_console: true,
+      drop_debugger: true,
     },
-    // 关闭文件计算
-    reportCompressedSize: false,
-    // 关闭生成map文件 可以达到缩小打包体积
-    sourcemap: false, // 这个生产环境一定要关闭，不然打包的产物会很大
-    // 压缩
-    minify: false,
-    cssCodeSplit: true,
-    rollupOptions: {
-      output: {
-        // 入口文件名
-        entryFileNames: `assets/js/[name]-[hash].js`,
-        // 块文件名
-        chunkFileNames: `assets/js/[name]-[hash].js`,
-        // 资源文件名 css 图片等等
-        assetFileNames: `assets/[ext]/[name]-[hash].[ext]`,
-        // 大文件拆分
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-             //把vue vue-router  @vueuse 等核心模块打包成一个文件
-            if (id.includes('vue')) {
-              return 'vue';
-            }else{
-              //最小化拆分包
-              return id
-                .toString()
-                .split("node_modules/")[1]
-                .split("/")[0]
-                .toString();
-            }
-            
-          }
-        },
+  },
+  rollupOptions: {
+    output: {
+      manualChunks: {
+        // 拆分代码，这个就是分包，配置完后自动按需加载，现在还比不上webpack的splitchunk，不过也能用了。
+        vue: ['vue', 'vue-router', 'pinia'],
+        vant: ['vant'],
+        nim: ['nim-web-sdk-ng'],
+        AgoraRTC: ['agora-rtc-sdk-ng'],
+        AgoraRTM: ['agora-rtm'],
       },
     },
   },
+},
 })
