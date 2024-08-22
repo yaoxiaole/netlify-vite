009df42154a1494c2cc65b55767cc2c0310f2a41
yaoxiaole <yaoniansong@163.com>
2024-08-22 18:17:42 +0800

    feat: update

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue + main" />
+  <HelloWorld msg="Vite + Vue + main123" />
 </template>
 
 <style scoped>
